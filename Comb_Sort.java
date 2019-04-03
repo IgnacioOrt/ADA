c1879289b91415ccce3ae712660c5b6ede6544e3
Aportelaher <hugo.aportela.20@gmail.com>
2019-04-03 11:51:14 -0600

    lol

--- Comb_Sort.java
+++ Comb_Sort.java
@@ -4,39 +4,32 @@ import java.util.*;
 
 class Comb_Sort 
 {
-    /* Function to merge the two haves arr[l..m] and 
-    arr[m+1..r] of array arr[] */
+
     static void combsort(int arr[], int n) 
     {
         int gap = arr.length;
         double factor = 1.3;
-        // Initialize swapped as true to make sure that 
-        // loop runs 
         boolean swapped = false;
 
-        // Keep running while gap is more than 1 and last 
-        // iteration caused a swap 
         while (gap > 1 || swapped == true) 
         { 
-            // Find next gap 
+ 
             if(gap > 1)
             {
                 gap = (int)(gap/factor);
             }
-            // Initialize swapped as false so that we can 
-            // check if swap happened or not 
+  
             swapped = false; 
-            // Compare all elements with current gap 
+
             for (int i=0; gap+i < arr.length; i++) 
             { 
                 if (arr[i] > arr[i+gap]) 
                 { 
-                    // Swap arr[i] and arr[i+gap] 
+
                     int temp = arr[i]; 
                     arr[i] = arr[i+gap]; 
                     arr[i+gap] = temp; 
   
-                    // Set swapped 
                     swapped = true; 
                 } 
             } 
@@ -51,7 +44,7 @@ class Comb_Sort
         System.out.printf("\n"); 
     } 
       
-    /* Driver program to test above functions */
+
     public static void main(String[] args) 
     { 
         Productor p = new Productor();
