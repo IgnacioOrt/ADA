a7c103220f8e5fbf6106ecdccbd6ac46954e47b8
Aportelaher <hugo.aportela.20@gmail.com>
2019-04-22 21:17:54 -0500

    Mochila 2

--- Practicas/Mochila.java
+++ Practicas/Mochila.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 public class Mochila
 {
-    static void bubbleSort(int[] arr) 
-    {
-
-    }  
-    static void printArray(int A[], int size) 
-    {
-
-
-    } 
     public static void main(String[] args) 
     {
       	Scanner input =new Scanner (System.in);
@@ -19,11 +10,65 @@ public class Mochila
         System.out.println("********Programa Mochila********");
         System.out.println("********************************\n");
 
-        System.out.println("Ingrese Cantidad Total de Objetos: ");
-        String totalObjetos = input.nextLine();
 
-        System.out.println("Ingrese el Peso Total que puede llevar la mochila: ");
+        System.out.println("Ingrese Cantidad Total de Objetos:");
+        int totalObjetos = input.nextInt();
+
+        while(totalObjetos <= 0)
+        {
+        		System.out.println("\nERROR!!! ");
+        		System.out.println("La Cantidad de Objetos tiene que ser mayor a 0 (Cero) ");
+        		System.out.println("Por favor, ingrese una cantidad aceptable de objetos");
+        		totalObjetos = input.nextInt();
+        }
+
+        System.out.println("Ingrese el Peso Total que puede llevar la mochila (de 50 a 100): ");
         int pesoMochila = input.nextInt();
+
+      	while(pesoMochila < 50 || pesoMochila > 100 )
+        {
+        		System.out.println("\nERROR!!! ");
+        		System.out.println("La Cantidad de Peso tiene que ser mayor a 50 (Cincuenta) y menor que 100 (Cien)");
+        		System.out.println("Por favor, ingrese una cantidad aceptable de Peso");
+        		pesoMochila = input.nextInt();
+        }
+
+			int [][] pesoObjeto = new int [totalObjetos][2];
+			System.out.println("\n");
+
+      		for(int i=0; i < totalObjetos;i++)
+        	{
+        	 	for(int j=0; j < 2;j++)
+        		{	
+        	 		if(j == 0)
+        	 		{
+        	 			System.out.println("Ingresa Peso (W) del Objeto "+(i+1)+":");
+        	 			pesoObjeto[i][j] = input.nextInt();
+        	 		}
+        	 		else
+        	 		{
+        	 			System.out.println("Ingresa Valor (V) del Objeto "+(i+1)+":");
+        	 			pesoObjeto[i][j] = input.nextInt();
+        	 		}
+        	 	}
+        	}
+
+
+			System.out.println("\n");
+        	for(int i=0; i < totalObjetos;i++)
+        	{
+        	 	for(int j=0; j < 2;j++)
+        		{	
+        	 		if(j == 0)
+        	 		{
+        	 			System.out.println("Peso del Objeto "+(i+1)+" (W) :"+pesoObjeto[i][j]);
+        	 		}
+        	 		else
+        	 		{
+        	 			System.out.println("Valor del Objeto "+(i+1)+" (V) :"+pesoObjeto[i][j]);
+        	 		}
+        	 	}
+        	}
 
 
     }
