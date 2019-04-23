c225091d7784c88bdbe45be146e0696e483e91f3
Aportelaher <hugo.aportela.20@gmail.com>
2019-04-22 19:44:44 -0500

    Inicio Mochila

---
+++ Practicas/Mochila.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Mochila
+{
+    static void bubbleSort(int[] arr) 
+    {
+
+    }  
+    static void printArray(int A[], int size) 
+    {
+
+    } 
+    public static void main(String[] args) 
+    {
+     System.out.println("Holis");
+    }
+}
