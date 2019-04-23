bdb4e552410f8542de5e867d436897b3b54961c4
Aportelaher <hugo.aportela.20@gmail.com>
2019-04-22 20:13:36 -0500

    Mochila 2

--- Practicas/Mochila.java
+++ Practicas/Mochila.java
@@ -9,9 +9,22 @@ public class Mochila
     static void printArray(int A[], int size) 
     {
 
+
     } 
     public static void main(String[] args) 
     {
-     System.out.println("Holis");
+      	Scanner input =new Scanner (System.in);
+
+        System.out.println("********************************");
+        System.out.println("********Programa Mochila********");
+        System.out.println("********************************\n");
+
+        System.out.println("Ingrese Cantidad Total de Objetos: ");
+        String totalObjetos = input.nextLine();
+
+        System.out.println("Ingrese el Peso Total que puede llevar la mochila: ");
+        int pesoMochila = input.nextInt();
+
+
     }
 }
