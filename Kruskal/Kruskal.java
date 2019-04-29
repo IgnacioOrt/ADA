6803d675578ab8277a5b8059a55536a759b53952
Aportelaher <hugo.aportela.20@gmail.com>
2019-04-28 22:25:32 -0500

    inicio

--- Kruskal/Kruskal.java
+++ Kruskal/Kruskal.java
@@ -2,10 +2,15 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Kruskal
-{
-	
+{	
+	public void Inicializar(int n)
+	{
+		for(int i=0; i < n; i++)
+		{
+			raiz[i] = i;
+		}
+	}
 	public static void main(String args[])
 	{
 
